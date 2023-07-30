6ef2a669a59e97f1cc98fcde10da7184b44871e5
Ricardo Fiel <rifiel@microsoft.com>
2023-07-31 00:45:18 +0100

    Update iis.ps1

--- dsc/iis.ps1
+++ dsc/iis.ps1
@@ -25,4 +25,4 @@
 # SOFTWARE.
 
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello VMSS 7 World from host $($env:computername) !"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello VMSS 8 World from host $($env:computername) !"
