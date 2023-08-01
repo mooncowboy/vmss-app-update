4cbff6b7dacc244978b33a86236156c18b64bb82
Ricardo Fiel <rifiel@microsoft.com>
2023-08-01 16:23:46 +0100

    iisreset

--- cse/updateapp.ps1
+++ cse/updateapp.ps1
@@ -1,3 +1,9 @@
+# Stop website so health probe stops sending traffic here
+iisreset /stop
+
 # Extract the published app to the webroot
 Expand-Archive -Path .\publishedapp.zip -Destination .
 Copy-Item -Path ".\publishedapp\**" -Destination "C:\inetpub\wwwroot" -Recurse -Force
+
+# Enable website again
+iisreset /start
