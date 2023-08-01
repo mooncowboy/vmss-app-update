cf8fe3770442057240ae1080e72cc928b61c0fd8
Ricardo Fiel <rifiel@microsoft.com>
2023-08-01 13:33:17 +0100

    Use APPVERSION in commandToExecute

--- cse/updateapp.ps1
+++ cse/updateapp.ps1
@@ -1,2 +1,2 @@
 # Extract the published app to the webroot
-Expand-Archive -Path publishedapp.zip -DestinationPath C:\inetpub\wwwroot -Force 
+Expand-Archive -Path .\publishedapp.zip -DestinationPath C:\inetpub\wwwroot -Force 
